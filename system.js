9eb1c7eadd445dbd4d1e79a82c1d90af05acb31f
orxan07 <orxan@rehimli.info>
2015-10-02 15:33:03 +0000

    first commit

---
+++ system.js
@@ -0,0 +1,19 @@
+// Define the system topology here. The topolgy should reference 
+// containers defined in definitions/services.js
+
+exports.name = 'demo';
+exports.namespace = 'demo';
+exports.id = 'f6db696e-88a6-4c47-88bb-dc3cb51b3a83';
+
+exports.topology = {
+  development: {
+  }
+};
+
+// Example
+//
+// exports.topology = {
+//   development: {
+//     root: ['web']
+//   }
+// };
