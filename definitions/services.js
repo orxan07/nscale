4f795d41a9e25168cfac142b0b83213519f87c1e
orxan07 <orxan@rehimli.info>
2015-10-08 14:35:53 +0000

    system compile

--- definitions/services.js
+++ definitions/services.js
@@ -16,7 +16,7 @@ exports.server = {
     repositoryUrl: 'https://github.com/orxan07/server.git',
     processBuild: 'npm install',
     execute: {
-      args: '-p 8080:8080 -d',
+      args: '-p 8000:8080 -d',
       process: 'src/server.js'
     }
   }
@@ -32,7 +32,7 @@ exports.client = {
     repositoryUrl: 'https://github.com/orxan07/client.git',
     processBuild: 'npm install',
     execute: {
-      args: '-p 1337:8080 -d',
+      args: '-p 1337:80 -d',
       process: 'src/server.js'
     }
   }
